a5dfaa86ad29887bebfe94a04bddea883a44c478
Pamela Fernandes Nilo <pamelanilo12@gmail.com>
2026-09-23 17:54:06 -0300

    first commit

---
+++ CandidatosTse/src/test/java/com/example/CandidatosTse/CandidatosTseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CandidatosTse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatosTseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
